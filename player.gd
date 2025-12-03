a422a5b0605d9efda02c0a726bd79819c0b81b6d
Brecht Kuppens <brecht.kuppens@gmail.com>
2025-12-03 23:37:46 +0100

    Simplified collision check for item pickup

--- player.gd
+++ player.gd
@@ -2,20 +2,17 @@ extends Node2D
 
 const WALK_SPEED := 40
 
-@onready var sprite = $Sprite2D
 @onready var carry_item_marker_2d = $CarryItemMarker2D
+@onready var hit_box_area_2d : Area2D = $HitBoxArea2D
 
-var item_to_pick_up : Node2D = null
 var item_carried : Node2D = null
 
 func _process(delta):
 	var walk := Input.get_axis("ui_left", "ui_right")
-
 	if walk < 0:
 		scale.x = -1
 	elif walk > 0:
 		scale.x = 1
-
 	position.x += walk * delta * WALK_SPEED
 	
 	if item_carried != null:
@@ -28,23 +25,15 @@ func _process(delta):
 func _unhandled_key_input(event: InputEvent):
 	if event.is_action_pressed("ui_accept") && item_carried && item_carried.has_method("use"):
 		item_carried.use()
-	elif event.is_action_pressed("pickup") && item_to_pick_up != null:
-		if item_carried != null:
-			item_carried.drop()
-			item_carried = null
-		item_to_pick_up.pickup()
-		item_carried = item_to_pick_up
-		item_to_pick_up = null
+	elif event.is_action_pressed("pickup"):
+		var overlapping_areas := hit_box_area_2d.get_overlapping_areas()
+		if overlapping_areas.size() == 1: # not handling overlapping items (yet)
+			if item_carried != null:
+				item_carried.drop()
+				item_carried = null
+			var item_to_pick_up : Node2D = overlapping_areas[0].get_parent()
+			item_to_pick_up.pickup()
+			item_carried = item_to_pick_up
 	elif event.is_action_pressed("drop") && item_carried != null:
 		item_carried.drop()
 		item_carried = null
-
-
-func _on_hit_box_area_2d_area_entered(area: Area2D):
-	if area.get_parent().has_method("pickup"):
-		item_to_pick_up = area.get_parent()
-
-func _on_hit_box_area_2d_area_exited(area: Area2D):
-	# note: this code has limitations. Might not handle overlapping items correctly.
-	if item_to_pick_up == area.get_parent():
-		item_to_pick_up = null
